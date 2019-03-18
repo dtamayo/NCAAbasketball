0ffb16cae7fd2e13aadd0fcd55f3399872fb8fd1
dtamayo <tamayo.daniel@gmail.com>
2019-03-17 21:53:27 -0400

    removed slots

--- holdings.py
+++ holdings.py
@@ -1,27 +1,25 @@
 import pandas as pd
 import numpy as np
 
-class Holdings():
-    def __init__(self, playins, field, teams):
-        self.array = np.zeros(68)
+class Calcutta():
+    def __init__(self, playins, field, teams, entries, pool):
         self.playins = playins
         self.field = field
         self.teams = teams
-        self.index = dict(zip(teams['TeamID'], teams.index))
-        self.team = []
+        self.entries = entries
+        self.pool = pool
+
+class Entry():
+    def __init__(self, bracket):
+        self.array = np.zeros(68)
+        self.teams = teams
+        self.index = dict(zip(bracket['TeamID'], bracket.index))
+        self.teams = []
     def add(self, teams, weight=1):
         if isinstance(teams, str):
             teams = [teams]
-        expteams = []
-        if 'field' in teams or 'Field' in teams:
-            expteams += list(self.field['Team'].values)
-        if 'Playin2' in teams:
-            expteams += list(self.playins.iloc[2:4]['Team'].values)
-        if 'Playin3' in teams:
-            expteams += list(self.playins.iloc[4:6]['Team'].values)
-        expteams += [team for team in teams if team != 'field' and team != 'Field' and team != 'Playin2' and team != 'Playin3']  
-        self.team += expteams
-        expteams = pd.DataFrame(expteams, columns=['Team'])
-        expteams['TeamID'] = expteams.apply(getID, axis=1)
-        for ID in expteams['TeamID']:
+        self.teams += teams
+        teams = pd.DataFrame(teams, columns=['Team'])
+        teams['TeamID'] = teams.apply(getID, axis=1)
+        for ID in teams['TeamID']:
             self.array[self.index[ID]] = weight
